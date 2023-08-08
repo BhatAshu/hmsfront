75d9b9600c0ed3906bde665464565a96d85bf094
Ashwini <ashwinibhat353@gmail.com>
2023-08-08 12:58:49 +0530

    aug-08

--- src/components/doctor/PatientDetails.js
+++ src/components/doctor/PatientDetails.js
@@ -1,133 +1,9 @@
-// import React, { useState } from "react";
-// import Modal from "@mui/material/Modal";
-// import Button from "@mui/material/Button";
-// import TextField from "@mui/material/TextField";
-// import Box from "@mui/material/Box";
-// import Typography from "@mui/material/Typography";
-
-// const modalStyle = {
-//     position: "absolute",
-//     top: "50%",
-//     left: "50%",
-//     transform: "translate(-50%, -50%)",
-//     width: "85%", // Increase modal width
-//     maxWidth: "600px", // Increase maximum modal width
-//     bgcolor: "white",
-//     boxShadow: 24,
-//     p: 4,
-//     borderRadius: "8px",
-//   };
-  
-//   const buttonContainerStyle = {
-//     display: "flex",
-//     justifyContent: "space-between",
-//     marginTop: "15px", // Add margin between button container and content
-//   };
-
-// const PatientDetailsModal = ({ open, onClose, patient }) => {
-//   const [activeSection, setActiveSection] = useState(null);
-//   if (!patient) {
-//     return null; // Return early if patient is null or undefined
-//   }
-
-//   const handleClose = () => {
-//     onClose();
-//   };
-
-//   const handleButtonClick = (section) => {
-//     setActiveSection(section);
-//   };
-
-//   const renderContent = () => {
-//     switch (activeSection) {
-//       case "patientDetails":
-//         return (
-//           <form>
-//             <TextField
-//               label="First Name"
-//               variant="outlined"
-//               value={patient.firstname}
-//               fullWidth
-//               disabled
-//             />
-//             <TextField
-//               label="Email"
-//               variant="outlined"
-//               value={patient.email}
-//               fullWidth
-//               disabled
-//             />
-//             <TextField
-//               label="Phone"
-//               variant="outlined"
-//               value={patient.phone}
-//               fullWidth
-//               disabled
-//             />
-//             <TextField
-//               label="Age"
-//               variant="outlined"
-//               value={patient.age}
-//               fullWidth
-//               disabled
-//             />
-//             <TextField
-//               label="Gender"
-//               variant="outlined"
-//               value={patient.gender}
-//               fullWidth
-//               disabled
-//             />
-//           </form>
-//         );
-//       default:
-//         return null;
-//     }
-//   };
-
-//   return (
-//     <Modal open={open} onClose={handleClose}>
-//       <Box sx={modalStyle}>
-//       <Typography variant="h5" gutterBottom color="black">
-//           {patient.firstname} Details
-//         </Typography>
-//         <div style={buttonContainerStyle}>
-//           <Button
-//             variant={
-//               activeSection === "patientDetails" ? "contained" : "outlined"
-//             }
-//             onClick={() => handleButtonClick("patientDetails")}
-//             sx={{ marginBottom: 2 }}
-//           >
-//             Patient Details
-//           </Button>
-//           <Button
-//             variant={
-//               activeSection === "medicalInformation" ? "contained" : "outlined"
-//             }
-//             onClick={() => handleButtonClick("medicalInformation")}
-//             sx={{ marginBottom: 2 }}
-//           >
-//             Medical Information
-//           </Button>
-//           <Button onClick={handleButtonClick}>Other Information</Button>
-//           <Button onClick={handleButtonClick}>Attachment</Button>
-//         </div>
-//         <Button onClick={handleClose} color="secondary">
-//           Close
-//         </Button>
-//         <Box mt={2}>{renderContent()}</Box>
-//       </Box>
-//     </Modal>
-//   );
-// };
-
-// export default PatientDetailsModal;
 import React, { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField"
 
 const modalStyle = {
     position: "absolute",
@@ -148,6 +24,13 @@ const modalStyle = {
   };
 const PatientDetailsModal = ({ open, onClose, patient }) => {
   const [activeSection, setActiveSection] = useState(null);
+  const [testRequestFields, setTestRequestFields] = useState({
+    name: "",
+    age: "",
+    chiefComplaint: "",
+    bloodGroup: "",
+    textarea: "",
+  });
   if (!patient) {
     return null; // Return early if patient is null or undefined
   }
@@ -157,6 +40,15 @@ const PatientDetailsModal = ({ open, onClose, patient }) => {
   const handleButtonClick = (section) => {
     setActiveSection(section);
   };
+
+  const handleTestRequestChange = (field, value) => {
+    setTestRequestFields((prevFields) => ({
+      ...prevFields,
+      [field]: value,
+    }));
+  };
+
+
   const renderContent = () => {
     switch (activeSection) {
       case "patientDetails":
@@ -191,8 +83,63 @@ const PatientDetailsModal = ({ open, onClose, patient }) => {
               {/* Add more medical information fields as needed */}
             </div>
           );
-        default:
-          return null;
+          case "testRequest":
+        return (
+          <div>
+            <div>
+              <TextField
+                label="Name"
+                value={patient.firstname}
+                fullWidth
+                margin="normal"
+                disabled
+              />
+              <TextField
+                label="Age"
+                value={patient.age}
+                fullWidth
+                margin="normal"
+                disabled
+              />
+            </div>
+            <TextField
+                label="ChiefComplaint"
+                value={patient.chiefcomplaint}
+                fullWidth
+                margin="normal"
+                disabled
+              />
+            {/* <TextField
+              label="Chief Complaint"
+              value={testRequestFields.chiefComplaint}
+              onChange={(e) =>
+                handleTestRequestChange("chiefComplaint", e.target.value)
+              }
+              fullWidth
+              margin="normal"
+            /> */}
+             <TextField
+                label="BloodGroup"
+                value={patient.bloodgroup}
+                fullWidth
+                margin="normal"
+                disabled
+              />
+            <TextField
+              label="Textarea"
+              value={testRequestFields.textarea}
+              onChange={(e) =>
+                handleTestRequestChange("textarea", e.target.value)
+              }
+              fullWidth
+              margin="normal"
+              multiline
+              rows={4}
+            />
+          </div>
+        );
+      default:
+        return null;
     }
   };
 
@@ -221,11 +168,20 @@ const PatientDetailsModal = ({ open, onClose, patient }) => {
           >
             Medical Information
           </Button>
-          <Button onClick={handleButtonClick}>Other Information</Button>
+          <Button
+            variant={
+              activeSection === "testRequest" ? "contained" : "outlined"
+            }
+            onClick={() => handleButtonClick("testRequest")}
+            sx={{ marginBottom: 2 }}
+          >
+            TestRequest
+          </Button>
+          {/* <Button onClick={handleButtonClick}>TestRequest</Button> */}
           <Button onClick={handleButtonClick}>Attachment</Button>
         </div>
         <Button onClick={handleClose} color="secondary">
-          Close
+          Back
         </Button>
         <Box mt={2}>{renderContent()}</Box>
       </Box>
